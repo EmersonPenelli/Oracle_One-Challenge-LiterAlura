0d7424f3d9ce93b1fcce373b10863995fffd9305
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-07-11 14:54:23 -0300

    Delete LiterAlura-Challenge directory

--- LiterAlura-Challenge/src/main/java/com/dev/LiterAlura_Challenge/Model/Services/DataConversion/DataConversion.java
+++
@@ -1,8 +0,0 @@
-package com.dev.LiterAlura_Challenge.Model.Services.DataConversion;
-
-public interface DataConversion {
-    <T> T convertData(String json, Class<T> clazz);
-
-    <T> T getSpecificKeyAndConvertData(String json, String nodeName, Class<T> clazz);
-
-}
